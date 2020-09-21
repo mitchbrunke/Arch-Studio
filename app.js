b3849b7057ebe83a06f01b31d9c16cc025de6b83
Mitch Brunke <mitchbrunke@Jordans-Air.modem>
2020-09-21 20:40:12 +1000

    for loop

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const numbers = document.querySelectorAll(".number");
 
+//create active class on number
+//loop through numbers
+
+numbers.forEach((number) => {
+  console.log(number);
+  //add Event Listener for each number
+  number.addEventListener("click", (e) => {
+    console.log("You clicked it");
+    //add active class
+    number.classList.add("active");
+  });
+});
+//remove active class??
+
+//use active class to display slide (switch maybe?)
