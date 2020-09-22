efde4318a25faffc8c25697688b806bbd37a2bc0
Mitch Brunke <mbrunke@fcte.com.au>
2020-09-22 16:32:17 +1000

    slider works

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
 const numbers = document.querySelectorAll(".number");
+const slideController = document.querySelector(".numbers");
+const playSlideOne = document.getElementById("one");
+const playSlideTwo = document.getElementById("two");
+const playSlideThree = document.getElementById("three");
+const playSlideFour = document.getElementById("four");
+const slideOne = document.querySelector(".slide1");
+const slideTwo = document.querySelector(".slide2");
+const slideThree = document.querySelector(".slide3");
+const slideFour = document.querySelector(".slide4");
 
-//create active class on number
-//loop through numbers
+//active class exists on slide 1. delete it
 
-numbers.forEach((number) => {
-  console.log(number);
-  //add Event Listener for each number
-  number.addEventListener("click", (e) => {
-    console.log("You clicked it");
-    //add active class
-    number.classList.add("active");
+removeActive = () =>
+  numbers.forEach((number) => {
+    number.classList.remove("active");
   });
-});
-//remove active class??
+
+// add active class on click
+
+addActive = (e) => {
+  removeActive();
+  e.target.classList.add("active");
+  activateSlide();
+};
 
 //use active class to display slide (switch maybe?)
+
+activateSlide = () => {
+  if (playSlideOne.classList.contains("active")) {
+    console.log("slide one is active");
+    slideOne.style.display = "flex";
+    slideTwo.style.display = "none";
+    slideThree.style.display = "none";
+    slideFour.style.display = "none";
+  } else if (playSlideTwo.classList.contains("active")) {
+    console.log("slide two is active");
+    slideOne.style.display = "none";
+    slideTwo.style.display = "flex";
+    slideThree.style.display = "none";
+    slideFour.style.display = "none";
+  } else if (playSlideThree.classList.contains("active")) {
+    console.log("slide three is active");
+    slideOne.style.display = "none";
+    slideTwo.style.display = "none";
+    slideThree.style.display = "flex";
+    slideFour.style.display = "none";
+  } else if (playSlideFour.classList.contains("active")) {
+    console.log("slide four is active");
+    slideOne.style.display = "none";
+    slideTwo.style.display = "none";
+    slideThree.style.display = "none";
+    slideFour.style.display = "flex";
+  }
+};
+//add event listeners
+
+slideController.addEventListener("click", addActive);
